089a46044dc2326804d53605e684579351050e73
qiuxan <qiuxan@gmail.com>
2023-10-06 08:00:06 +1100

    Data Fetching  Back End

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  // fetch users from https://jsonplaceholder.typicode.com/users
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>UsersPage</h1>
+      <ul>
+        {users.map((user: User) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
