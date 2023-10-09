a805ae40fdbb0c7037541ec07899b1912a3a7ec6
qiuxan <qiuxan@gmail.com>
2023-10-09 22:00:06 +1100

    Layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavigationBar from "./NavigationBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavigationBar />
+
+        <main className="p-5">{children}</main>
+      </body>
     </html>
   );
 }
