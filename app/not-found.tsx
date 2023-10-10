b7f4996689274222ff92e3f33a47b1b09b5d5ae5
qiuxan <qiuxan@gmail.com>
2023-10-10 21:21:47 +1100

    Handling Not Found Errors

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  // BEGIN: ed8c6549bwf9
+  return <div>The requested page doesn&rsquo;t exist.</div>;
+  // END: ed8c6549bwf9
+};
+
+export default NotFoundPage;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  // BEGIN: ed8c6549bwf9
+  return <div>this user doesn&rsquo;t exist.</div>;
+  // END: ed8c6549bwf9
+};
+
+export default UserNotFoundPage;
