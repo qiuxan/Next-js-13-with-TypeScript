ab2a7a2ab7e711a519f31db884ca5d0d11c5c928
qiuxan <qiuxan@gmail.com>
2023-10-11 17:20:37 +1100

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -13,3 +13,26 @@ export function GET(
   }
   return NextResponse.json({ id: 1, name: "John Doe" });
 }
+
+// create a put function
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  //to do:
+  //1. validate the request body
+  //2, if invalid, return a 400 response
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "name is requied" }, { status: 400 });
+  //3, fetch the user from the database with the given id
+  //4, if the user doesn't exist, return a 404 response
+  if (params.id > 10) {
+    //mocking the database
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  //5, update the user with the request body
+  //6, return a 200 response with the updated user
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
