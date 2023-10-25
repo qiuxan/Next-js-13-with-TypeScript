9101ad0c3c4366f8a75519c4fb1e526c777f8a9e
qiuxan <qiuxan@gmail.com>
2023-10-26 09:22:35 +1100

    Signing Out Users

--- app/NavigationBar.tsx
+++ app/NavigationBar.tsx
@@ -12,7 +12,11 @@ const NavigationBar = () => {
       </Link>
       <Link href="/users">Users</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div>{session?.user?.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session?.user?.name} <Link href="/api/auth/signout">Sign Out</Link>
+        </div>
+      )}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
       )}
