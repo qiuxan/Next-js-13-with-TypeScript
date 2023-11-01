9e6343c8c6bf8b91b00e97f32a082309006d36f9
qiuxan <qiuxan@gmail.com>
2023-11-01 21:35:35 +1100

    Lazy Loading

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeavyComponent = () => {
+  return <div>My Heavy Component</div>;
+};
+
+export default HeavyComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import coffee from "@/public/images/coffee.jpg";
+// import HeavyComponent from "./components/HeavyComponent";
+import dynamic from "next/dynamic";
+// import _ from "lodash";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
+import { useState } from "react";
 
-export default async function Home() {
+export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <main className="relative h-screen">
       {/* <Image
@@ -18,6 +29,32 @@ export default async function Home() {
       >
         Hello world
       </h1>
+      <button
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        Show
+      </button>
+      {isVisible && <HeavyComponent />}
+
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [
+            { name: "cohn", age: 20 },
+            { name: "bane", age: 30 },
+            { name: "aoe", age: 40 },
+          ];
+          const sortedUsers = _.sortBy(users, "name");
+          console.log(
+            "🚀 ~ file: page.tsx:49 ~ Home ~ sortedUsers:",
+            sortedUsers
+          );
+        }}
+      >
+        Log Array
+      </button>
     </main>
   );
 }
