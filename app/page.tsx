4a3de69794d4222fe38975aa55e3ac9290ddfb25
qiuxan <qiuxan@gmail.com>
2023-11-01 19:21:14 +1100

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import coffee from "@/public/images/coffee.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users"> Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        fill
+        src="https://bit.ly/react-cover"
+        alt="coffee image"
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 30vw"
+        quality={80}
+        priority
+      />
     </main>
   );
 }
